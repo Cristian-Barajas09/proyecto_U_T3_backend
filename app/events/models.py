20825b815623f7b7372abc2c1f2239de47b963bb
Cristian Alejandro Barajas Bolivar <cristianbarajasimasc@gmail.com>
2024-04-28 23:34:51 -0400

    feat: create endpoints for events app

--- app/events/models.py
+++ app/events/models.py
@@ -7,14 +7,19 @@ class EventManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(deleted_at__isnull=True)
 
+class EventAllManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()
+
 class Event(models.Model):
 
     logic = EventManager()
+    objects = EventAllManager()
 
     title = models.CharField(max_length=200)
-    image = models.URLField()
-    description = models.TextField()
-    date = models.DateField()
+    image = models.URLField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    date = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
@@ -26,6 +31,9 @@ class Event(models.Model):
         self.deleted_at = timezone.now()
         self.save()
 
+    def restore(self):
+        self.deleted_at = None
+        self.save()
     @dataclass
     class Meta:
         db_table = 'events'
