9f19f054af5921528ca21008636041491b36a5f6
Frank Duran <dmfrankjosue@gmail.com>
2024-04-30 18:46:09 -0400

    feat/inventory/9/create-model-plates-django

--- app/inventory/models.py
+++ app/inventory/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Plates(models.Model):
+    "Plate of table"
+    name = models.CharField(max_length=60, null=False)
+    description = models.TextField(null=False)
+    image = models.URLField()
+    price = models.FloatField(max_length=30,null=False)
+
