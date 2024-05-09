9e8230ef4ed9c3de85513dd3a34964377ade29fa
Frank Duran <dmfrankjosue@gmail.com>
2024-05-08 21:16:19 -0400

    add model plates

--- app/inventory/models.py
+++ app/inventory/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Plates(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    image = models.URLField()
+    price = models.DecimalField(max_digits=5,max_length=10)
