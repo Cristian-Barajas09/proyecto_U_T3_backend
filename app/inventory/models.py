ab6c613e21643cc9bf87530044c86635ecc25aa7
Bayron Beltran <bayronlarreal@gmail.com>
2024-05-10 18:01:52 -0400

    update

--- app/inventory/models.py
+++ app/inventory/models.py
@@ -4,7 +4,7 @@ class Plates(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
     image = models.URLField()
-    price = models.DecimalField(max_digits=5,max_length=10)
+    price = models.DecimalField(max_digits=5,max_length=10,decimal_places=2)
     create_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
