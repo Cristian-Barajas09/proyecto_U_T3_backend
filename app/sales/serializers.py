1c6bd43407de92e191b9d08a4e4c18cb6d9df9ee
Cristian Alejandro Barajas Bolivar <cristianbarajasimasc@gmail.com>
2024-05-22 11:55:24 -0400

    feat: Update URLs for sales and events apps
    The URLs for the sales and events apps have been updated to follow a more consistent and organized structure. This change improves the readability and maintainability of the codebase.

--- app/sales/serializers.py
+++ app/sales/serializers.py
@@ -1,22 +1,38 @@
+"""This module contains the serializers for the sales app."""
+from dataclasses import dataclass
 from rest_framework import serializers
 from .models import PayMethod, Order, OrderPlates, Payment
 
 class PayMethodSerializers(serializers.ModelSerializer):
+    """This module contains the serializers for the sales app."""
+    @dataclass
     class Meta:
+        """This class contains the metadata for the PayMethodSerializer."""
         model = PayMethod
         fields = ('id','currency','method')
 
 class OrderSerializers(serializers.ModelSerializer):
+    """This module contains the serializers for the sales app."""
+    @dataclass
     class Meta:
-        model: Order
+        """This class contains the metadata for the OrderSerializer."""
+        model = Order
         fields= '__all__'
 
+
 class OrderPlatesSerializers(serializers.ModelSerializer):
+    """This module contains the serializers for the sales app."""
+    @dataclass
     class Meta:
-        model: OrderPlates
+        """This class contains the metadata for the OrderPlatesSerializer."""
+        model = OrderPlates
         fields='__all__'
 
+
 class PaymentSerializers(serializers.ModelSerializer):
+    """This module contains the serializers for the sales app."""
+    @dataclass
     class Meta:
-        model: Payment
+        """This class contains the metadata for the PaymentSerializer."""
+        model =  Payment
         fields= '__all__'
--- app/sales/urls.py
+++ app/sales/urls.py
@@ -1,3 +1,4 @@
+"""Urls for sales app"""
 from rest_framework import routers
 from .views import OrderViewset, OrderPlatesViewset, PayMethodViewset, PaymentViewset
 
@@ -5,12 +6,10 @@ route = routers.DefaultRouter()
 
 route.register('orders', OrderViewset , 'orders')
 
-route.register ('OrderPlates', OrderPlatesViewset, 'OrderPlates')
+route.register ('order/plates', OrderPlatesViewset, 'OrderPlates')
 
 route.register ('paymethods', PayMethodViewset, 'paymethods')
 
 route.register ('payments', PaymentViewset, 'payments')
 
 urlpatterns = route.urls
-
-
