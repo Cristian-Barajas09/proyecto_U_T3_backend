d5d55e4775542e9f27bfc30f8632a25f809f7517
Cristian Alejandro Barajas Bolivar <cristianbarajasimasc@gmail.com>
2024-05-24 22:13:03 -0400

    feat: Register Plates, Category, and Ingredient models in admin site
    This commit registers the Plates, Category, and Ingredient models in the admin site, allowing administrators to manage these models through the Django admin interface.

--- app/inventory/serializers.py
+++ app/inventory/serializers.py
@@ -1,7 +1,33 @@
+"""Serializers for inventory app"""
+from dataclasses import dataclass
 from rest_framework import serializers
-from .models import Plates
+from .models import Plates,Category,Ingredient
 
 class PlatesSerializers(serializers.ModelSerializer):
+    """Serializer for Plates"""
+    categories = serializers.StringRelatedField(many=True)
+    ingredients = serializers.StringRelatedField(many=True)
+    @dataclass
     class Meta:
+        """Meta class for PlatesSerializers"""
         model = Plates
-        fields = ('id','title','description','image','price',)
+        fields = ('id','title','description','image','price','categories','ingredients')
+
+
+class IngredientSerializers(serializers.ModelSerializer):
+    """Serializer for Ingredient"""
+
+    @dataclass
+    class Meta:
+        """Meta class for IngredientSerializers"""
+        model = Ingredient
+        fields = ('id','title','description')
+
+class CategorySerializers(serializers.ModelSerializer):
+    """Serializer for Category"""
+
+    @dataclass
+    class Meta:
+        """Meta class for CategorySerializers"""
+        model = Category
+        fields = ('id','title','description')
